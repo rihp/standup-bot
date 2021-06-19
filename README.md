e1e0b999a61c6fbc79a602a1f17fa4789754d178
Roberto <roberto@henriquezperozo.com>
2021-06-19 23:24:24 +0200

    basic set up

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # standup-bot
+
+
+1. Set up basic discord bot `hello world` 
+2. Set up cronjobs for the `hello world`
+3. Send DMs following cronjob times
+4. Set up mongoose
+5. Save DM replies in mongodb
+6. Send DM replies to server with custom chron expression
---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict"; // since I hate not using semicolons
+
+/**
+ * Required Imports
+ *  - dotenv: .env support
+ *  - fs: file system support (for reading ./commands)
+ *  - mongoose: mongoDB client
+ *  - discord.js: discord (duh)
+ *  - schedule: for running the cron jobs
+ *  - standup.model: the model for the standup stored in mongo
+ */
+require("dotenv").config();
+//const fs = require("fs");
+//const mongoose = require("mongoose");
+//const { Client, MessageEmbed, Collection } = require("discord.js");
+//const schedule = require("node-schedule");
+//const standupModel = require("./models/standup.model");
+//const showPromptCommand = require("./commands/showPrompt");
+//const messsages = require(process.env.MESSAGES)
+//const PREFIX = process.env.PREFIX
+
+// _RIHP_
+//
+// simulate .env file
+//const params = require("./params");
+//import * as params from './params.js'
+console.log(process.env.PREFIX)
+console.log("this is how we do it")
